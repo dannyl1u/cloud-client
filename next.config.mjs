b9322653689a9a392f0eea64411fb98498ec347f
Danny Liu <dannyjialiliu@gmail.com>
2024-03-12 23:16:33 -0700

    Update next.config.mjs with basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 const nextConfig = {
     output: 'export',
-    assetPrefix: 'https://storage.googleapis.com/webui-client/out',
+    basePath: '/out',
+    assetPrefix: 'https://storage.googleapis.com/webui-client/out/',
 };
   
 export default nextConfig;
